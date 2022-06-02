864842efc4273cf1818d926f01d28225b20224e6
Jeivisson <jeivisson@ymail.com>
2022-06-02 18:41:35 -0300

    TextComponent done

--- src/components/Dummy/styles.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-import { css } from 'styled-components';
-
-export const Container = styled.div`
-   ${({ theme }) => css``}
-`;
--- src/components/Heading/Heading.test.jsx
+++ src/components/Heading/Heading.test.jsx
@@ -90,4 +90,10 @@ describe('<Heading />', () => {
 
     expect(h6.tagName.toLowerCase()).toBe('h6');
   });
+  it('should match snapshot', () => {
+    const { container } = renderTheme(<Heading>texto</Heading>);
+    const heading = screen.getByRole('heading', { name: 'texto' });
+
+    expect(container).toMatchSnapshot();
+  });
 });
