a6d23b4f9d03957ae14452dac5539a71f51c5628
Jeivisson <jeivisson@ymail.com>
2022-06-21 18:10:01 -0300

    fix coverage background

--- src/components/GrindText/GrindText.test.jsx
+++ src/components/GrindText/GrindText.test.jsx
@@ -3,8 +3,14 @@ import { renderTheme } from '../../styles/render-theme';
 import mock from './mock';
 
 describe('<GrindSection>', () => {
-  it('should render', () => {
+  it('should render with background', () => {
     const { container } = renderTheme(<GrindText {...mock} />);
+    expect(container).toMatchSnapshot();
+  });
+  it('should render without background', () => {
+    const { container } = renderTheme(
+      <GrindText {...mock} background={undefined} />,
+    );
     expect(container).toMatchSnapshot();
   });
 });
