635cc0594c120cfdce80c7925a2d77fca1bfa277
Jeivisson <jeivisson@ymail.com>
2022-06-29 16:00:51 -0300

    add comp GridImage/Footer with jest test

--- src/components/GridImage/GridImage.test.jsx
+++ src/components/GridImage/GridImage.test.jsx
@@ -1,15 +1,15 @@
-import { GrindText } from '.';
+import { GridImage } from '.';
 import { renderTheme } from '../../styles/render-theme';
 import mock from './mock';
 
-describe('<GrindText>', () => {
+describe('<GrindImage>', () => {
   it('should render with background', () => {
-    const { container } = renderTheme(<GrindText {...mock} />);
+    const { container } = renderTheme(<GridImage {...mock} />);
     expect(container).toMatchSnapshot();
   });
   it('should render without background', () => {
     const { container } = renderTheme(
-      <GrindText {...mock} background={undefined} />,
+      <GridImage {...mock} background={undefined} />,
     );
     expect(container).toMatchSnapshot();
   });
